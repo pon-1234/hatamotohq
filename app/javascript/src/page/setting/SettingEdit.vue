6c57971b6b2713ccf484a16bdb2f2d05102bf480
Giap Nguyen <giapnh@yopaz.vn>
2021-08-31 18:19:15 +0700

    Style fixes on setting page

--- app/javascript/src/page/setting/SettingEdit.vue
+++ app/javascript/src/page/setting/SettingEdit.vue
@@ -7,68 +7,66 @@
         </a>
         <h5 class="m-auto font-weight-bold">設定変更</h5>
       </div>
-      <ValidationObserver ref="observer" v-slot="{ invalid, validate }">
+      <ValidationObserver ref="observer" v-slot="{ validate }">
         <form ref="form" @submit.prevent="validate().then(onSubmit)" :action="getAction()" method="post" enctype="multipart/form-data">
           <input type="hidden" name="authenticity_token" :value="csrfToken">
           <input type="hidden" name="_method" value="patch" />
           <input type="hidden" name="setting[id]" :value="settingFormData.id">
           <div class="card-body">
-            <div class="card">
-              <div class="form-group row">
-                <label class="col-12">LINE公式アカウントID<required-mark/></label>
-                <div class="col-12">
-                  <ValidationProvider name="LINE公式アカウントID" rules="required" v-slot="{ errors }">
-                    <input type="text" class="form-control" name="setting[line_user_id]" placeholder="input" v-model="settingFormData.line_user_id">
-                    <span class="error-explanation">{{ errors[0] }}</span>
-                  </ValidationProvider>
-                </div>
+            <div class="form-group row">
+              <label class="col-12">LINE公式アカウントID<required-mark/></label>
+              <div class="col-12">
+                <ValidationProvider name="LINE公式アカウントID" rules="required" v-slot="{ errors }">
+                  <input type="text" class="form-control" name="setting[line_user_id]" placeholder="入力してください" v-model="settingFormData.line_user_id">
+                  <span class="error-explanation">{{ errors[0] }}</span>
+                </ValidationProvider>
               </div>
-              <div class="form-group row">
-                <label class="col-12">LINEアカウント名<required-mark/></label>
-                <div class="col-12">
-                  <ValidationProvider name="LINEアカウント名" rules="required" v-slot="{ errors }">
-                    <input type="text" class="form-control" name="setting[line_name]" placeholder="input" v-model="settingFormData.line_name">
-                    <span class="error-explanation">{{ errors[0] }}</span>
-                  </ValidationProvider>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">LINEアカウント名<required-mark/></label>
+              <div class="col-12">
+                <ValidationProvider name="LINEアカウント名" rules="required" v-slot="{ errors }">
+                  <input type="text" class="form-control" name="setting[line_name]" placeholder="入力してください" v-model="settingFormData.line_name">
+                  <span class="error-explanation">{{ errors[0] }}</span>
+                </ValidationProvider>
               </div>
-              <div class="form-group row">
-                <label class="col-12">表示名<required-mark/></label>
-                <div class="col-12">
-                  <ValidationProvider name="表示名" rules="required" v-slot="{ errors }">
-                    <input type="text" class="form-control" name="setting[display_name]" placeholder="input" v-model="settingFormData.display_name">
-                    <span class="error-explanation">{{ errors[0] }}</span>
-                  </ValidationProvider>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">表示名<required-mark/></label>
+              <div class="col-12">
+                <ValidationProvider name="表示名" rules="required" v-slot="{ errors }">
+                  <input type="text" class="form-control" name="setting[display_name]" placeholder="入力してください" v-model="settingFormData.display_name">
+                  <span class="error-explanation">{{ errors[0] }}</span>
+                </ValidationProvider>
               </div>
-              <div class="form-group row">
-                <label class="col-12">チャネルID</label>
-                <div class="col-12">
-                  <input type="text" class="form-control" placeholder="input" v-model="settingFormData.line_channel_id" disabled>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">チャネルID</label>
+              <div class="col-12">
+                <input type="text" class="form-control" placeholder="入力してください" v-model="settingFormData.line_channel_id" disabled>
               </div>
-              <div class="form-group row">
-                <label class="col-12">チャネルシークレット</label>
-                <div class="col-12">
-                  <input type="text" class="form-control" placeholder="input" v-model="settingFormData.line_channel_secret" disabled>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">チャネルシークレット</label>
+              <div class="col-12">
+                <input type="text" class="form-control" placeholder="入力してください" v-model="settingFormData.line_channel_secret" disabled>
               </div>
-              <div class="form-group row">
-                <label class="col-12">Webhook URL</label>
-                <div class="col-12">
-                  <input type="text" class="form-control" placeholder="input" :value="WebhookUrl()" disabled>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">Webhook URL</label>
+              <div class="col-12">
+                <input type="text" class="form-control" placeholder="入力してください" :value="WebhookUrl()" disabled>
               </div>
-              <div class="form-group row">
-                <label class="col-12">チャネルシークレット</label>
-                <div class="col-12">
-                  <input type="text" class="form-control" placeholder="input" v-model="settingFormData.liff_id" disabled>
-                </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-12">LIFF ID</label>
+              <div class="col-12">
+                <input type="text" class="form-control" placeholder="入力してください" v-model="settingFormData.liff_id" disabled>
               </div>
             </div>
           </div>
           <div class="card-footer row-form-btn d-flex">
-            <submit-button object="設定" action="保存" :submitted="submitted" :disabled="invalid"></submit-button>
+            <submit-button object="設定" action="保存" :submitted="submitted"></submit-button>
           </div>
         </form>
       </ValidationObserver>
