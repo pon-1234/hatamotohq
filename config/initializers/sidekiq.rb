7ec4b8e35aa0843ab386b312a0437b854c3ec447
Giap Nguyen <giapnh@yopaz.vn>
2021-09-10 09:24:29 +0700

    Add sidekiq.rb

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Sidekiq::Extensions.enable_delay!
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
+end
