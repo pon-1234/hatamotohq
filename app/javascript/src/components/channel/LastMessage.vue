de84e6ced9f0b42ed26efab34b407f7b1cf9afda
Giap Nguyen <giapnh@yopaz.vn>
2021-10-07 15:12:20 +0700

    Truncate last message displayed on channel list

--- app/javascript/src/components/channel/LastMessage.vue
+++ app/javascript/src/components/channel/LastMessage.vue
@@ -1,15 +1,23 @@
 <template>
-  <span>{{ message || 'メッセージなし' }}</span>
+  <span class="text-prewrap max-1-lines">{{
+    truncatedMessage || "メッセージなし"
+  }}</span>
 </template>
 
 <script>
 export default {
   props: {
     message: String
+  },
+
+  computed: {
+    truncatedMessage() {
+      return _.truncate(
+        this.message, {
+          length: 15
+        }
+      );
+    }
   }
 };
 </script>
-
-<style>
-
-</style>
