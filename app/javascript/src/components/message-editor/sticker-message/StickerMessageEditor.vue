412db22001d495b7511925b3eb06bfdec060a340
Giap Nguyen <giapnh@yopaz.vn>
2021-10-07 13:55:36 +0700

    Minor fixes on sticker message editor

--- app/javascript/src/components/message-editor/sticker-message/StickerMessageEditor.vue
+++ app/javascript/src/components/message-editor/sticker-message/StickerMessageEditor.vue
@@ -1,44 +1,60 @@
 <template>
-  <div >
+  <div>
     <div class="border" v-if="packageId && stickerId">
-      <div class="figure figure-grid figure-grid-sm mr-3 mb-3 position-relative">
+      <div
+        class="figure figure-grid figure-grid-sm mr-3 mb-3 position-relative"
+      >
         <img
-          :src="'https://stickershop.line-scdn.net/stickershop/v1/sticker/'+ stickerId+'/PC/sticker.png'"
+          :src="`https://stickershop.line-scdn.net/stickershop/v1/sticker/${stickerId}/PC/sticker.png`"
           alt="tabImage"
         />
         <button
           type="button"
           class="position-absolute close"
-          style="top: 10px; right: 0px;"
+          style="top: 10px; right: 0px"
           @click="removeSticker"
         >
-
           <i class="mdi mdi-close-outline"></i>
           <!-- <i class="fas fa-times"></i> -->
         </button>
       </div>
     </div>
     <div v-else>
-      <div class="border text-center" :class="errors.first('sticker-id' + index)?'invalid-box':'' ">
+      <div
+        class="border text-center"
+        :class="errors.first('sticker-id' + index) ? 'invalid-box' : ''"
+      >
         <div class="figure figure-grid-sm my-4">
           <a
             class="text-primary"
             href="#"
             data-toggle="modal"
-            data-target="#stickerModalCenter"
-            @click="onShowModal()"
-          >スタンプを選択
+            :data-target="`#stickerModalCenter${index}`"
+            @click="showStickerModal()"
+            >スタンプを選択
           </a>
           <div class="text-muted opacity-30">
             <i class="mdi mdi-sticker-emoji mdi-3x"></i>
           </div>
         </div>
       </div>
-      <error-message :message="errors.first('sticker-id' + index)"></error-message>
+      <error-message
+        :message="errors.first('sticker-id' + index)"
+      ></error-message>
     </div>
     <!-- Modal -->
-    <modal-select-sticker ref="modalSticker" name="stickerModalCenter" @input="selectSticker" />
-    <input type="hidden" v-model="stickerId" :name="'sticker-id' + index" v-validate="'required'" data-vv-as="スタンプ" />
+    <modal-select-sticker
+      :ref="`modalSticker${index}`"
+      :id="`stickerModalCenter${index}`"
+      @input="selectSticker"
+    />
+    <input
+      type="hidden"
+      v-model="stickerId"
+      :name="'sticker-id' + index"
+      v-validate="'required'"
+      data-vv-as="スタンプ"
+    />
   </div>
 </template>
 <script>
@@ -56,8 +72,8 @@ export default {
     selectSticker(sticker) {
       this.$emit('input', sticker);
     },
-    onShowModal() {
-      this.$refs.modalSticker.shownModal();
+    showStickerModal() {
+      this.$refs[`modalSticker${this.index}`].reset();
     }
   }
 };
