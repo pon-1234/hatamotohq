f86d56f2d45e8042f5c91bd3e075f78f683291af
Giap Nguyen <giapnh@yopaz.vn>
2021-10-18 11:21:28 +0700

    Minor fix on survey form

--- app/javascript/src/components/survey/preview/SurveyPreviewContent.vue
+++ app/javascript/src/components/survey/preview/SurveyPreviewContent.vue
@@ -3,7 +3,7 @@
     <div class="font-weight-bold">{{ survey.title }}</div>
     <div class="font-12">{{ survey.description }}</div>
 
-    <div v-for="(question, index) in questions" :key="index">
+    <div class="mt-2" v-for="(question, index) in questions" :key="index">
       <!-- Input text question -->
       <survey-form-text :question="question" :qnum="index + 1" v-if="question.type === 'text'"></survey-form-text>
 
