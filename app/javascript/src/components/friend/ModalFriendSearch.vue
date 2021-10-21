398085ef8a84424a6d740ac6a0b748bb775f88f3
Giap Nguyen <giapnh@yopaz.vn>
2021-10-21 19:46:16 +0700

    Fix typos

--- app/javascript/src/components/friend/ModalFriendSearch.vue
+++ app/javascript/src/components/friend/ModalFriendSearch.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="modalFriendSearch" ref="modalFriendSearch" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="info-header-modalLabel" aria-hidden="true">
+  <div
+    id="modalFriendSearch"
+    ref="modalFriendSearch"
+    class="modal fade"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="info-header-modalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog modal-lg" :key="contentKey">
       <div class="modal-content">
         <div class="modal-header">
@@ -11,24 +19,28 @@
           <div class="row form-group">
             <div class="col-lg-4">名前</div>
             <div class="col-lg-8">
-              <input type="text" id="name" v-model="keyword" class="form-control" placeholder="LINE登録名、表示名から検索" maxlength="64">
+              <input
+                type="text"
+                id="name"
+                v-model="keyword"
+                class="form-control"
+                placeholder="LINE登録名、表示名から検索"
+                maxlength="64"
+              />
             </div>
           </div>
           <!-- タグ検索 -->
           <div class="row form-group">
             <div class="col-lg-4">タグ</div>
             <div class="col-lg-8">
-              <input-tag :tags="selectedTags" @input="onSelectTags" :allTags="true"/>
+              <input-tag :tags="selectedTags" @input="onSelectTags" :allTags="true" />
             </div>
           </div>
           <!-- 登録日時 -->
           <div class="row form-group">
             <div class="col-lg-4">登録日時</div>
             <div class="col-lg-8">
-              <daterange-picker
-                class="d-block"
-                :start_date.sync="created_at_gteq"
-                :end_date.sync="created_at_lteq">
+              <daterange-picker class="d-block" :start_date.sync="created_at_gteq" :end_date.sync="created_at_lteq">
               </daterange-picker>
             </div>
           </div>
@@ -46,12 +58,26 @@
           <!-- ブロック、非表示した友達 -->
           <div class="mt-2 d-flex">
             <div class="flex-1 custom-control custom-checkbox mr-2">
-              <input type="checkbox" class="custom-control-input" id="lockedFriendCheck" v-model="locked_eq" :true-value="null" :false-value="false">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="lockedFriendCheck"
+                v-model="locked_eq"
+                :true-value="null"
+                :false-value="false"
+              />
               <label class="custom-control-label" for="lockedFriendCheck">ブロックした人を表示</label>
             </div>
 
             <div class="flex-1 custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="hiddenFriendCheck" v-model="visible_eq" :true-value="null" :false-value="true">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="hiddenFriendCheck"
+                v-model="visible_eq"
+                :true-value="null"
+                :false-value="true"
+              />
               <label class="custom-control-label" for="hiddenFriendCheck">非表示した人を表示</label>
             </div>
           </div>
@@ -61,9 +87,12 @@
           <button type="button" class="btn btn-sm btn-light ml-auto" data-dismiss="modal">キャンセル</button>
           <button type="button" class="btn btn-sm btn-info" data-dismiss="modal" @click="search">検索</button>
         </div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+  <!-- /.modal -->
 </template>
 <script>
 import { mapActions, mapMutations, mapState } from 'vuex';
@@ -72,7 +101,7 @@ export default {
     return {
       contentKey: 0,
       selectedTags: [],
-      queryParamsPages: {}
+      params: {}
     };
   },
   mounted() {
@@ -80,57 +109,76 @@ export default {
   },
   computed: {
     ...mapState('friend', {
-      queryParams: (state) => state.queryParams,
-      clearQueryParams: (state) => state.clearQueryParams
+      queryParams: state => state.queryParams,
+      clearQueryParams: state => state.clearQueryParams
     }),
     keyword: {
-      get() { return this.queryParamsPages.line_name_or_display_name_cont; },
-      set(value) { this.queryParamsPages.line_name_or_display_name_cont = value; }
+      get() {
+        return this.params.line_name_or_display_name_cont;
+      },
+      set(value) {
+        this.params.line_name_or_display_name_cont = value;
+      }
     },
 
     tags: {
-      get() { return this.queryParamsPages.tags_id_in || []; },
+      get() {
+        return this.params.tags_id_in || [];
+      },
       set(value) {
         const tagIds = value.map(_ => _.id);
-        this.queryParamsPages.tags_id_in = tagIds;
+        this.params.tags_id_in = tagIds;
       }
     },
 
     status_eq: {
-      get() { return this.queryParamsPages.status_eq; },
-      set(value) { this.queryParamsPages.status_eq = value; }
+      get() {
+        return this.params.status_eq;
+      },
+      set(value) {
+        this.params.status_eq = value;
+      }
     },
 
     created_at_gteq: {
-      get() { return this.queryParamsPages.created_at_gteq; },
-      set(value) { this.queryParamsPages.created_at_gteq = value; }
+      get() {
+        return this.params.created_at_gteq;
+      },
+      set(value) {
+        this.params.created_at_gteq = value;
+      }
     },
 
     created_at_lteq: {
-      get() { return this.queryParamsPages.created_at_lteq; },
-      set(value) { this.queryParamsPages.created_at_lteq = value; }
+      get() {
+        return this.params.created_at_lteq;
+      },
+      set(value) {
+        this.params.created_at_lteq = value;
+      }
     },
 
     locked_eq: {
-      get() { return this.queryParamsPages.locked_eq; },
-      set(value) { this.queryParamsPages.locked_eq = value; }
+      get() {
+        return this.params.locked_eq;
+      },
+      set(value) {
+        this.params.locked_eq = value;
+      }
     },
 
     visible_eq: {
-      get() { return this.queryParamsPages.visible_eq; },
-      set(value) { this.queryParamsPages.visible_eq = value; }
+      get() {
+        return this.params.visible_eq;
+      },
+      set(value) {
+        this.params.visible_eq = value;
+      }
     }
   },
   methods: {
-    ...mapMutations('friend', [
-      'setQueryParams',
-      'setQueryParam',
-      'resetQueryParams',
-      'setClearQueryParams'
-    ]),
-    ...mapActions('friend', [
-      'getFriends'
-    ]),
+    ...mapMutations('friend', ['setQueryParams', 'setQueryParam', 'resetQueryParams', 'setClearQueryParams']),
+    ...mapActions('friend', ['getFriends']),
     forceRerender() {
       this.contentKey++;
     },
@@ -139,7 +187,7 @@ export default {
       this.selectedTags = tags;
     },
     search() {
-      this.setQueryParams(this.queryParamsPages);
+      this.setQueryParams(this.params);
       this.getFriends();
     },
     resetSearch() {
@@ -153,7 +201,7 @@ export default {
         this.setClearQueryParams(false);
       }
       this.forceRerender();
-      this.queryParamsPages = _.cloneDeep(this.queryParams);
+      this.params = _.cloneDeep(this.queryParams);
     }
   }
 };
