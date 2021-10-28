bc72bd13b88b47bf2641489b3b8523adb879afa0
Giap Nguyen <giapnh@yopaz.vn>
2021-10-28 14:04:54 +0700

    Hide divider if need

--- app/javascript/src/components/broadcast/BroadcastDeliverTarget.vue
+++ app/javascript/src/components/broadcast/BroadcastDeliverTarget.vue
@@ -3,11 +3,11 @@
   <div v-else-if="broadcast.type === 'condition'">
     <div v-if="broadcast.tags && broadcast.tags.length > 0" class="d-flex align-items-center flex-wrap">
       <span class="text-sm mr-2">タグ</span>
-      <span v-for="(tag, index) in broadcast.tags" :key="index" class="badge badge-warning badge-pill pt-1">{{
+      <span v-for="(tag, index) in broadcast.tags" :key="index" class="badge badge-warning badge-pill">{{
         tag.name
       }}</span>
-      <divider></divider>
     </div>
+    <divider v-if="broadcast.conditions && broadcast.conditions.type === 'specific'"></divider>
     <div
       v-if="broadcast.conditions && broadcast.conditions.type === 'specific'"
       class="d-flex align-items-center flex-wrap"
