8f104a0346d9fb63f3a0973a3b0d17063afc4055
trungvdh <trungvdh@yopaz.vn>
2021-11-01 14:54:04 +0700

    Show image richmenu when creating image richmenu

--- app/javascript/src/components/media/MediaIndex.vue
+++ app/javascript/src/components/media/MediaIndex.vue
@@ -182,7 +182,7 @@ export default {
   props: {
     types: {
       type: Array,
-      default: () => ['image', 'audio', 'video']
+      default: () => ['image', 'audio', 'video', 'richmenu']
     },
     mode: {
       type: String,
