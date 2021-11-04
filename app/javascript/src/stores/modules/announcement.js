e9d3f735fca9f69c41fed6e6b54f9d120e5452bf
trungvdh <trungvdh@yopaz.vn>
2021-11-04 15:35:38 +0700

    Show announcements when page load

--- app/javascript/src/stores/modules/announcement.js
+++ app/javascript/src/stores/modules/announcement.js
@@ -48,6 +48,7 @@ export const actions = {
     };
     try {
       const response = await AnnouncementApi.list(params);
+      console.log(response);
       context.commit('setAnnouncements', response.data);
       context.commit('setMeta', response.meta);
       return response;
@@ -63,6 +64,7 @@ export const actions = {
     };
     try {
       const response = await AnnouncementApi.published(params);
+      console.log(response);
       context.commit('setAnnouncements', response.data);
       context.commit('setMeta', response.meta);
       return response;
