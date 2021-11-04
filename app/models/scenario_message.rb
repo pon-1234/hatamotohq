ff0e7e63c539f443902b720e224475403fb8d85d
Giap Nguyen <giapnh@yopaz.vn>
2021-11-04 13:53:44 +0700

    Update scenario_message.rb: add null check

--- app/models/scenario_message.rb
+++ app/models/scenario_message.rb
@@ -49,7 +49,7 @@ class ScenarioMessage < ApplicationRecord
 
   private
     def execute_before_save
-      if date == 0 && time.to_time.seconds_since_midnight == 0
+      if date == 0 && time.to_time&.seconds_since_midnight == 0
         self.is_initial = true
       end
     rescue StandardError => e
