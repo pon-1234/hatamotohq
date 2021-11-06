91d638601b27ccc994304028a0a9fb9087b00573
Giap Nguyen <giapnh@yopaz.vn>
2021-11-06 22:28:49 +0700

    Update sidekiq config

--- app/jobs/worker_get_number_of_message_delivery.rb
+++ app/jobs/worker_get_number_of_message_delivery.rb
@@ -5,22 +5,25 @@ class WorkerGetNumberOfMessageDelivery
 
   def perform(*args)
     LineAccount.active.each do |line_account|
+      ActiveJob::Base.logger.info "Start WorkerGetNumberOfMessageDelivery Account ID = #{line_account.id}"
       next if line_account.channel_id.nil?
-      response = LineApi::GetNumberOfMessageDeliveries.new(line_account).perform(Time.zone.today)
+      response = LineApi::GetNumberOfMessageDeliveries.new(line_account).perform(Time.zone.yesterday)
       next unless response.code == '200'
-      data = response.body
-      insight = Insight.find_or_initialize_by(line_account: line_account, type: :daily, date: Time.zone.today)
-      insight.broadcast = data.try(:broadcast)
-      insight.targeting = data.try(:targeting)
-      insight.auto_response = data.try(:autoResponse)
-      insight.welcome_response = data.try(:welcomeResponse)
-      insight.chat = data.try(:chat)
-      insight.api_broadcast = data.try(:apiBroadcast)
-      insight.api_push = data.try(:apiPush)
-      insight.api_multicast = data.try(:apiMulticast)
-      insight.api_narrowcast = data.try(:apiNarrowcast)
-      insight.api_reply = data.try(:apiReply)
+      data = JSON.parse(response.body)
+      ActiveJob::Base.logger.info "Response WorkerGetNumberOfMessageDelivery #{data}"
+      insight = Insight.find_or_initialize_by(line_account: line_account, type: :daily, date: Time.zone.yesterday)
+      insight.broadcast = data["broadcast"]
+      insight.targeting = data["targeting"]
+      insight.auto_response = data["autoResponse"]
+      insight.welcome_response = data["welcomeResponse"]
+      insight.chat = data["chat"]
+      insight.api_broadcast = data["apiBroadcast"]
+      insight.api_push = data["apiPush"]
+      insight.api_multicast = data["apiMulticast"]
+      insight.api_narrowcast = data["apiNarrowcast"]
+      insight.api_reply = data["apiReply"]
       insight.save!
+      ActiveJob::Base.logger.info "End WorkerGetNumberOfMessageDelivery Account ID = #{line_account.id}"
     end
   end
 end
