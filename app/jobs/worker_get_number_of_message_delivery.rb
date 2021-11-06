7762fed8b756efc94506d6562896909da7d02f99
Giap Nguyen <giapnh@yopaz.vn>
2021-11-06 21:38:11 +0700

    Minor fixes

--- app/jobs/worker_get_number_of_message_delivery.rb
+++ app/jobs/worker_get_number_of_message_delivery.rb
@@ -12,14 +12,14 @@ class WorkerGetNumberOfMessageDelivery
       insight = Insight.find_or_initialize_by(line_account: line_account, type: :daily, date: Time.zone.today)
       insight.broadcast = data.try(:broadcast)
       insight.targeting = data.try(:targeting)
-      insight.auto_response = data.try(:auto_response)
-      insight.welcome_response = data.try(:welcome_response)
+      insight.auto_response = data.try(:autoResponse)
+      insight.welcome_response = data.try(:welcomeResponse)
       insight.chat = data.try(:chat)
-      insight.api_broadcast = data.try(:api_broadcast)
-      insight.api_push = data.try(:api_push)
-      insight.api_multicast = data.try(:api_multicast)
-      insight.api_narrowcast = data.try(:api_narrowcast)
-      insight.api_reply = data.try(:api_reply)
+      insight.api_broadcast = data.try(:apiBroadcast)
+      insight.api_push = data.try(:apiPush)
+      insight.api_multicast = data.try(:apiMulticast)
+      insight.api_narrowcast = data.try(:apiNarrowcast)
+      insight.api_reply = data.try(:apiReply)
       insight.save!
     end
   end
