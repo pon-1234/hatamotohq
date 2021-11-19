263fb108d82026301e21f610df7634f4d4e5f717
Giap Nguyen <giapnh@yopaz.vn>
2021-11-19 18:19:18 +0700

    Remove unused tables

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_080627) do
+ActiveRecord::Schema.define(version: 2021_11_19_111427) do
   create_table 'action_objects', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci', force: :cascade do |t|
     t.string 'title'
     t.text 'description'
