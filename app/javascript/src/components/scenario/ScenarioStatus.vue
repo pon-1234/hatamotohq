36ede7914c8711b8c06db49d433db33a3356316e
Giap Nguyen <giapnh@yopaz.vn>
2021-11-23 11:08:11 +0700

    Minor fixes

--- app/javascript/src/components/scenario/ScenarioStatus.vue
+++ app/javascript/src/components/scenario/ScenarioStatus.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <span v-if="status === 'enabled'"><i  class='mdi mdi-circle text-primary'></i> 稼働中</span>
-    <span v-else-if="status === 'disabled'"><i  class='mdi mdi-circle text-warning'></i> 停止中</span>
-    <span v-else-if="status === 'draft'"><i  class='mdi mdi-circle text-secondary'></i> 下書き</span>
+    <span v-if="status === 'enabled'"><i class="mdi mdi-circle text-success"></i> 稼働中</span>
+    <span v-else-if="status === 'disabled'"><i class="mdi mdi-circle text-warning"></i> 停止中</span>
+    <span v-else-if="status === 'draft'"><i class="mdi mdi-circle text-secondary"></i> 下書き</span>
   </div>
 </template>
 
