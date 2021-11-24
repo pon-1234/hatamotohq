5cfea5d218864971956ef38ba988b4469adc6f7a
Giap Nguyen <giapnh@yopaz.vn>
2021-11-24 09:56:47 +0700

    Add episodes_count to reminder table

---
+++ app/javascript/src/filters/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import moment from 'moment-timezone';
+
+// Custom filters
+Vue.filter('formatted_time', (value) => {
+  if (!value) return '';
+  value = value.toString();
+  return moment(value).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH:mm');
+});
--- app/views/user/friends/set_reminder.json.jbuilder
+++ app/views/user/friends/set_reminder.json.jbuilder
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 json.name @reminding.reminder.name
 json.goal @reminding.goal
 json.created_at @reminding.created_at
