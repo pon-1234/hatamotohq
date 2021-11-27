1dd09a3e0f86a9b802ea13073cb21d73e9fce371
Giap Nguyen <giapnh@yopaz.vn>
2021-11-27 19:07:42 +0700

    Only sync friends if line_account is configured

--- app/workers/daily_sync_friends_worker.rb
+++ app/workers/daily_sync_friends_worker.rb
@@ -5,6 +5,7 @@ class DailySyncFriendsWorker
 
   def perform(*args)
     LineAccount.all.each do |line_account|
+      next if line_account.channel_id.nil?
       SyncLineFriendsJob.perform_later(line_account.id)
     end
   end
