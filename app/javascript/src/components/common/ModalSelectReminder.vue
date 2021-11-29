fac9f6b48c6416d1b21df4e35c5724b7551bd421
Giap Nguyen <giapnh@yopaz.vn>
2021-11-29 16:34:58 +0700

    Show all reminders on selection modal

--- app/javascript/src/components/common/ModalSelectReminder.vue
+++ app/javascript/src/components/common/ModalSelectReminder.vue
@@ -7,7 +7,7 @@
     aria-labelledby="myModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+    <div class="modal-dialog modal-lg vh-90 modal-dialog-scrollable" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h4 class="modal-title">リマインダを選択してください</h4>
@@ -15,7 +15,7 @@
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body overflow-hidden">
+        <div class="modal-body">
           <div class="d-flex" v-if="folders && folders.length">
             <folder-left
               type="reminder"
