3ed1fccdfad3b4d14203a558e7f7f6408f4b3368
Giap Nguyen <giapnh@yopaz.vn>
2021-12-15 09:37:24 +0700

    Typo fixes

--- app/javascript/src/components/client/ClientStatus.vue
+++ app/javascript/src/components/client/ClientStatus.vue
@@ -1,6 +1,6 @@
 <template>
   <span v-if="client.status == 'active'"><i class="mdi mdi-circle text-success"></i> 有効 </span>
-  <span v-else-if="client.status == 'blocked'"><i class="mdi mdi-circle text-warning"></i> ブロックした </span>
+  <span v-else-if="client.status == 'blocked'"><i class="mdi mdi-circle text-warning"></i> 無効 </span>
 </template>
 
 <script>
