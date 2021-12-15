81bd08c19baa67e7ebccbd751149c2175b245f15
Giap Nguyen <giapnh@yopaz.vn>
2021-12-15 17:55:46 +0700

    Update user ability

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -4,54 +4,56 @@ class UserAbility
   include CanCan::Ability
 
   def initialize(user)
-    can :read, LineFriend, line_account: user.line_account
-    can :manage, Channel, line_account: user.line_account
-    return unless user.admin?
-    # Friend
-    can [:manage], LineFriend, line_account: user.line_account
+    if user.staff?
+      can :read, LineFriend, { line_account: user.line_account, channel: { assignee_id: user.id } }
+      can :manage, Channel, { line_account: user.line_account, assignee_id: user.id }
+    elsif user.admin?
+      # Friend
+      can [:manage], LineFriend, line_account: user.line_account
 
-    return unless user.admin?
-    can :manage, User, line_account: user.line_account
+      return unless user.admin?
+      can :manage, User, line_account: user.line_account
 
-    # Broadcast
-    can [:manage], Broadcast, line_account: user.line_account
-    can [:create], Broadcast
+      # Broadcast
+      can [:manage], Broadcast, line_account: user.line_account
+      can [:create], Broadcast
 
-    # Scenario
-    can [:manage], Scenario, line_account: user.line_account
-    can [:create], Scenario
+      # Scenario
+      can [:manage], Scenario, line_account: user.line_account
+      can [:create], Scenario
 
-    # Auto Response
-    can [:manage], AutoResponse, line_account: user.line_account
-    can [:create], AutoResponse
+      # Auto Response
+      can [:manage], AutoResponse, line_account: user.line_account
+      can [:create], AutoResponse
 
-    # Channel
-    can [:read, :create, :update], Channel, line_account: user.line_account
-    can :create_message, Channel, { line_account: user.line_account, locked: false }
+      # Channel
+      can [:read, :create, :update], Channel, line_account: user.line_account
+      can :create_message, Channel, { line_account: user.line_account, locked: false }
 
-    # RichMenu
-    can [:manage], RichMenu, line_account: user.line_account
-    can [:create], RichMenu
+      # RichMenu
+      can [:manage], RichMenu, line_account: user.line_account
+      can [:create], RichMenu
 
-    # Media
-    can [:manage], Media, line_account: user.line_account
-    can [:create], Media
+      # Media
+      can [:manage], Media, line_account: user.line_account
+      can [:create], Media
 
-    # Friend
-    can [:manage], LineFriend, line_account: user.line_account
+      # Friend
+      can [:manage], LineFriend, line_account: user.line_account
 
-    # Folder
-    can [:manage], Folder, line_account: user.line_account
-    can [:create], Folder
+      # Folder
+      can [:manage], Folder, line_account: user.line_account
+      can [:create], Folder
 
-    # Tag
-    can [:manage], Tag, folder: { line_account: user.line_account }
-    can [:create], Tag
+      # Tag
+      can [:manage], Tag, folder: { line_account: user.line_account }
+      can [:create], Tag
 
-    # Reminder
-    can [:manage], Reminder, folder: { line_account: user.line_account }
-    can [:create], Reminder
-    can [:manage], Episode, reminder: { line_account: user.line_account }
-    can [:create], Episode
+      # Reminder
+      can [:manage], Reminder, folder: { line_account: user.line_account }
+      can [:create], Reminder
+      can [:manage], Episode, reminder: { line_account: user.line_account }
+      can [:create], Episode
+    end
   end
 end
