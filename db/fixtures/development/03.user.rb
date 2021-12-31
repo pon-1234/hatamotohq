ddddd5365319339f833e9843a88440483a38161c
Giap Nguyen <giapnh@yopaz.vn>
2021-12-31 10:40:31 +0700

    Update user seeder

--- db/fixtures/development/03.user.rb
+++ db/fixtures/development/03.user.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+# Schema sometimes need to be reloaded before running a seed
+User.reset_column_information()
 User.seed do |s|
   s.id = 1
   s.email = 'client@example.com'
   s.password = 'Admin1234@'
   s.name = 'クライアント管理者'
   s.role = 'admin'
+  s.client = Client.first
 end
