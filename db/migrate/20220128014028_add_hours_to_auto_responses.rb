f8340eac3a50eefc51b2180035fbf2c71f22a853
Giap Nguyen <giapnh@yopaz.vn>
2022-02-15 18:54:17 +0700

    Set default value for auto_response's biz_hours

--- db/migrate/20220128014028_add_hours_to_auto_responses.rb
+++ db/migrate/20220128014028_add_hours_to_auto_responses.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class AddHoursToAutoResponses < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :auto_responses, :biz_hours, :json, after: :status
+    AutoResponse.find_in_batches do |auto_responses|
+      auto_responses.each do |auto_response|
+        auto_response.biz_hours = {
+          enabled: false,
+          weekdays: [],
+          is_all_day: false,
+          time: {
+            start: '00:00',
+            end: '23:59'
+          }
+        }
+        auto_response.save!
+      end
+    end
+  end
+
+  def down
+    remove_column :auto_responses, :biz_hours
   end
 end
