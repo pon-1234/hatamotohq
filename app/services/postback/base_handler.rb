0c41e8dea37558e94273a8ac016be219646b1342
tranhuyhoangbka <hoangth92.nd@gmail.com>
2022-03-02 19:24:09 +0700

    fix display errors, wrong message sender in review page

--- app/services/postback/base_handler.rb
+++ app/services/postback/base_handler.rb
@@ -7,7 +7,7 @@ class Postback::BaseHandler
   end
 
   def send_text_message(text)
-    message = Messages::MessageBuilder.new(@friend, @friend.channel, { message: { type: 'text', text: text } }.try(:with_indifferent_access)).perform
+    message = Messages::MessageBuilder.new(nil, @friend.channel, { message: { type: 'text', text: text } }.try(:with_indifferent_access)).perform
     LineApi::PushMessage.new(@friend.line_account).perform([message.content], @friend.line_user_id)
   end
 end
