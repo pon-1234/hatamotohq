d91520fd3c19617b74696d975d06c79b40612245
trungvdh <trungvdh@yopaz.vn>
2022-03-14 16:19:59 +0700

    Fix query n+1

--- app/helpers/user/friends_helper.rb
+++ app/helpers/user/friends_helper.rb
@@ -8,7 +8,7 @@ module User::FriendsHelper
     headers = %w(id line_user_id line_name display_name line_picture_url status note created_at)
     friends.each do |friend|
       row = export_for_row_hash(headers, friend)
-      row['tags'] = friend.tags.pluck(:name).join(',')
+      row['tags'] = friend.tags.collect(&:name).join(',')
       rows << row
     end
     rows
