3754ceb272edbf95f385d0196dfdda7c2be17059
hoangth1992 <hoangth@yopaz.vn>
2022-06-15 17:46:18 +0700

    fix stream route ui

--- app/javascript/src/page/user/stream-route/stream-route-form.vue
+++ app/javascript/src/page/user/stream-route/stream-route-form.vue
@@ -7,135 +7,144 @@
     >
       <input type="hidden" name="authenticity_token" :value="csrfToken" />
       <div>
-        <div class="card">
-          <div class="card-body">
-            <div class="form-border">
-              <ValidationProvider
-                :name="'流入経路名称'"
-                :rules="'required|max:255'"
-                v-slot="{ errors }"
-              >
-                <div class="form-group">
-                  <label>流入経路名称<required-mark /></label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    maxlength="255"
-                    placeholder="流入経路名称を入力してください"
-                    v-model.trim="streamRouteFormData.name"
-                  />
-                  <span class="error-explanation">{{ errors[0] }}</span>
-                </div>
-              </ValidationProvider>
-            </div>
-            <div class="form-border">
-              <ValidationProvider
-                :name="'QRコード表示用テキスト'"
-                :rules="'max:16'"
-                v-slot="{ errors }"
-              >
-                <div class="form-group">
-                  <label>QRコード表示用テキスト</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    maxlength="16"
-                    placeholder="QRコード表示用テキストを入力してください"
-                    v-model.trim="streamRouteFormData.qr_title"
-                  />
-                  <span class="error-explanation">{{ errors[0] }}</span>
-                </div>
-              </ValidationProvider>
-            </div>
+        <div class="mxw-1200">
+          <div class="card">
+            <div class="card-body">
+              <div class="form-border">
+                <ValidationProvider
+                  :name="'流入経路名称'"
+                  :rules="'required|max:255'"
+                  v-slot="{ errors }"
+                >
+                  <div class="form-group">
+                    <label>流入経路名称<required-mark /></label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      maxlength="255"
+                      placeholder="流入経路名称を入力してください"
+                      v-model.trim="streamRouteFormData.name"
+                    />
+                    <span class="error-explanation">{{ errors[0] }}</span>
+                  </div>
+                </ValidationProvider>
+              </div>
+              <div class="form-border">
+                <ValidationProvider
+                  :name="'QRコード表示用テキスト'"
+                  :rules="'max:16'"
+                  v-slot="{ errors }"
+                >
+                  <div class="form-group">
+                    <label>QRコード表示用テキスト</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      maxlength="16"
+                      placeholder="QRコード表示用テキストを入力してください"
+                      v-model.trim="streamRouteFormData.qr_title"
+                    />
+                    <span class="error-explanation">{{ errors[0] }}</span>
+                  </div>
+                </ValidationProvider>
+              </div>
 
-            <div class=" form-group">
-              <div class="form-group">
-                <div class="has-modal-xl">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <action-editor-custom
-                        :requiredLabel="requiredLabel"
-                        :showTitle="showTitle"
-                        :showLaunchMessage="false"
-                        :value="streamRouteFormData.actionData ? streamRouteFormData.actionData[0] : {}"
-                        @input="updateAction"
-                      />
+              <div class=" form-group">
+                <div class="form-group">
+                  <div class="has-modal-xl">
+                    <div class="row">
+                      <div class="col-sm-8">
+                        <action-editor-custom
+                          :requiredLabel="requiredLabel"
+                          :showTitle="showTitle"
+                          :showLaunchMessage="false"
+                          :value="streamRouteFormData.actionData ? streamRouteFormData.actionData[0] : {}"
+                          @input="updateAction"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div class="row">
-              <div class="col-sm-8">
-                <label for="run_add_friend_actions">友だち追加時設定</label>
-                <div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
-                    <label @click="changeRunAddFriendActions(false)" :class="`${!streamRouteFormData.run_add_friend_actions ? 'active' : ''} btn btn-primary d-flex justify-content-center prevent-default-label`">
-                      <input
-                        type="radio"
-                        name="run_add_friend_actions"
-                        :value="false"
-                        autocomplete="off"
-                        :checked="!streamRouteFormData.run_add_friend_actions"
-                      > 無視する
-                    </label>
-                    <label @click="changeRunAddFriendActions(true)" :class="`${streamRouteFormData.run_add_friend_actions ? 'active' : ''} btn btn-primary text-center d-flex justify-content-center prevent-default-label`">
-                      <input
-                        type="radio"
-                        name="run_add_friend_actions"
-                        :value="true"
-                        autocomplete="off"
-                        :checked="streamRouteFormData.run_add_friend_actions"
-                      > 無視しない
-                    </label>
+              <div class="row">
+                <div class="col-sm-8">
+                  <label for="run_add_friend_actions">
+                    友だち追加時設定
+                    <tool-tip
+                      title="無視する... 流入経路から友だち追加された場合、「友だち追加時設定」のアクションを無視します。
+                             無視しない... 「友だち追加時設定」のアクションが反映された後にこの流入経路のアクションが反映されます。
+                             「無視する」「無視しない」の設定に関わらず、流入経路のアクションでシナリオを操作している場合、「友だち追加時設定」のシナリオは配信されません。">
+                    </tool-tip>
+                  </label>
+                  <div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+                      <label @click="changeRunAddFriendActions(false)" :class="`${!streamRouteFormData.run_add_friend_actions ? 'active' : ''} btn btn-primary d-flex justify-content-center prevent-default-label`">
+                        <input
+                          type="radio"
+                          name="run_add_friend_actions"
+                          :value="false"
+                          autocomplete="off"
+                          :checked="!streamRouteFormData.run_add_friend_actions"
+                        > 無視する
+                      </label>
+                      <label @click="changeRunAddFriendActions(true)" :class="`${streamRouteFormData.run_add_friend_actions ? 'active' : ''} btn btn-primary text-center d-flex justify-content-center prevent-default-label`">
+                        <input
+                          type="radio"
+                          name="run_add_friend_actions"
+                          :value="true"
+                          autocomplete="off"
+                          :checked="streamRouteFormData.run_add_friend_actions"
+                        > 無視しない
+                      </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row mt-3">
+                <div class="col-sm-8">
+                  <label for="always_run_actions">アクションの実行</label>
+                  <div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+                      <label @click="changeAlwaysRunActions(true)" :class="`${streamRouteFormData.always_run_actions ? 'active' : ''} btn btn-primary d-flex justify-content-center prevent-default-label`">
+                        <input
+                          type="radio"
+                          name="always_run_actions"
+                          :value="false"
+                          autocomplete="off"
+                          :checked="streamRouteFormData.always_run_actions"
+                        > いつでも
+                      </label>
+                      <label @click="changeAlwaysRunActions(false)" :class="`${!streamRouteFormData.always_run_actions ? 'active' : ''} btn btn-primary text-center d-flex justify-content-center prevent-default-label`">
+                        <input
+                          type="radio"
+                          name="always_run_actions"
+                          :value="true"
+                          autocomplete="off"
+                          :checked="!streamRouteFormData.always_run_actions"
+                        > 初回の友だち追加時のみ
+                      </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-border mt-3">
+                <div class="form-group">
+                  <label class="fw-300">フォルダー</label>
+                  <div class="flex-grow-1">
+                    <select v-model="streamRouteFormData.folder_id" class="form-control fw-300">
+                      <option v-for="(folder, index) in folder_options" :key="index" :value="folder.id">
+                        {{ folder.name }}
+                      </option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div class="row mt-3">
-              <div class="col-sm-8">
-                <label for="always_run_actions">アクションの実行</label>
-                <div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
-                    <label @click="changeAlwaysRunActions(true)" :class="`${streamRouteFormData.always_run_actions ? 'active' : ''} btn btn-primary d-flex justify-content-center prevent-default-label`">
-                      <input
-                        type="radio"
-                        name="always_run_actions"
-                        :value="false"
-                        autocomplete="off"
-                        :checked="streamRouteFormData.always_run_actions"
-                      > いつでも
-                    </label>
-                    <label @click="changeAlwaysRunActions(false)" :class="`${!streamRouteFormData.always_run_actions ? 'active' : ''} btn btn-primary text-center d-flex justify-content-center prevent-default-label`">
-                      <input
-                        type="radio"
-                        name="always_run_actions"
-                        :value="true"
-                        autocomplete="off"
-                        :checked="!streamRouteFormData.always_run_actions"
-                      > 初回の友だち追加時のみ
-                    </label>
-                </div>
-              </div>
+            <div class="card-footer text-center">
+              <button type="submit" class="btn btn-primary mw-120"><strong>保存</strong></button>
             </div>
 
-            <div class="form-border mt-3">
-              <div class="form-group">
-                <label class="fw-300">フォルダー</label>
-                <div class="flex-grow-1">
-                  <select v-model="streamRouteFormData.folder_id" class="form-control fw-300">
-                    <option v-for="(folder, index) in folder_options" :key="index" :value="folder.id">
-                      {{ folder.name }}
-                    </option>
-                  </select>
-                </div>
-              </div>
-            </div>
+            <loading-indicator :loading="loading"></loading-indicator>
           </div>
-          <div class="card-footer text-center">
-            <button type="submit" class="btn btn-primary mw-120"><strong>保存</strong></button>
-          </div>
-
-          <loading-indicator :loading="loading"></loading-indicator>
         </div>
       </div>
     </form>
