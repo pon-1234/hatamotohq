fffa214754c09a57fad27a1e8dc689da7422561b
Giap Nguyen <giapnh@yopaz.vn>
2022-09-25 11:09:46 +0700

    Sync survey response to googlesheet

--- app/jobs/append_survey_response_to_google_sheet_job.rb
+++ app/jobs/append_survey_response_to_google_sheet_job.rb
@@ -3,7 +3,23 @@
 class AppendSurveyResponseToGoogleSheetJob < ApplicationJob
   queue_as :default
 
-  def perform(survey_response)
-    # TODO
+  def perform(response_id)
+    response = SurveyResponse.find(response_id)
+    survey = response.survey
+    # Init gooogle service
+    sheets = Google::Apis::SheetsV4::SheetsService.new
+    sheets.authorization = survey.ggapi_auth_tokens['access_token']
+
+    questions = survey.survey_questions
+    answers = response.survey_answers.map { |x| x.norm_answer }
+    data = [response.id, response.created_at, response.line_friend_id, response.line_friend_name] + answers
+    values = [
+      data
+    ]
+    value_range = Google::Apis::SheetsV4::ValueRange.new(values: values)
+    result = sheets.append_spreadsheet_value(survey.spreadsheet_id,
+                                              "A1:A#{data.size}",
+                                              value_range,
+                                              value_input_option: 'RAW')
   end
 end
