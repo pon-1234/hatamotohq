805820e42067a7ad1dc5a4e3e81205727346191d
Giap Nguyen <giapnh@yopaz.vn>
2023-03-22 10:52:48 +0700

    Reorder action precheckin

--- app/javascript/src/core/constant.js
+++ app/javascript/src/core/constant.js
@@ -296,10 +296,10 @@ export const PostbackTypes = {
   tag: 'タグ操作',
   reminder: 'リマインダ操作',
   scoring: 'スコアリング操作',
-  precheckin: '予約・事前チェックイン',
   rsv_intro: '予約・紹介送信',
   rsv_cancel_intro: '予約・空室待ちキャンセル',
   rsv_contact: '予約・お問い合わせ',
+  precheckin: '予約・事前チェックイン',
   // service_review: 'サービス評価フォーム送信',
   assign_staff: '担当者割り当て',
   none: '何もしない'
