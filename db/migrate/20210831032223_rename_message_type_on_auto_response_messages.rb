b2ab6bc5e141d52b4bb9402b1ba36655c9027079
pon <mamepon@gmail.com>
2025-06-07 12:41:21 +0900

    Fix: PostgreSQL compatible type conversion in migration

--- db/migrate/20210831032223_rename_message_type_on_auto_response_messages.rb
+++ db/migrate/20210831032223_rename_message_type_on_auto_response_messages.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class RenameMessageTypeOnAutoResponseMessages < ActiveRecord::Migration[6.0]
-  def change
+  def up
     rename_column :auto_response_messages, :message_type, :message_type_id
-    change_column :auto_response_messages, :message_type_id, :integer
+    execute <<-SQL
+      ALTER TABLE auto_response_messages 
+      ALTER COLUMN message_type_id TYPE integer 
+      USING message_type_id::integer
+    SQL
+  end
+
+  def down
+    change_column :auto_response_messages, :message_type_id, :string
+    rename_column :auto_response_messages, :message_type_id, :message_type
   end
 end
