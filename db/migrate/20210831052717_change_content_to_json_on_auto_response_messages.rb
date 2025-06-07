a6f91d4b1a916127fbc3180ccf3970e67c0cc24e
pon <mamepon@gmail.com>
2025-06-07 12:43:28 +0900

    Fix: PostgreSQL compatible JSON type conversion in migration

--- db/migrate/20210831052717_change_content_to_json_on_auto_response_messages.rb
+++ db/migrate/20210831052717_change_content_to_json_on_auto_response_messages.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ChangeContentToJsonOnAutoResponseMessages < ActiveRecord::Migration[6.0]
-  def change
-    change_column :auto_response_messages, :content, :json
+  def up
+    execute <<-SQL
+      ALTER TABLE auto_response_messages 
+      ALTER COLUMN content TYPE json 
+      USING content::json
+    SQL
+  end
+
+  def down
+    change_column :auto_response_messages, :content, :text
   end
 end
