56179fc5fe37ead9353afb308b8a294bc3a4f8b5
pon <mamepon@gmail.com>
2025-06-07 12:49:43 +0900

    Fix: PostgreSQL compatible boolean type conversion in migration

--- db/migrate/20210918051830_change_column_selected_of_richmenu_to_boolean.rb
+++ db/migrate/20210918051830_change_column_selected_of_richmenu_to_boolean.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class ChangeColumnSelectedOfRichmenuToBoolean < ActiveRecord::Migration[6.0]
-  def change
-    change_column :rich_menus, :selected, :boolean
+  def up
+    execute <<-SQL
+      ALTER TABLE rich_menus 
+      ALTER COLUMN selected TYPE boolean 
+      USING CASE 
+        WHEN selected = 'true' OR selected = '1' THEN true
+        WHEN selected = 'false' OR selected = '0' THEN false
+        ELSE NULL
+      END
+    SQL
+  end
+
+  def down
+    change_column :rich_menus, :selected, :string
   end
 end
